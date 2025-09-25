680f5b59d11190352702044c4041d18721ffa904
Shaheer kayani <shaheer.kayani8@gmail.com>
2025-09-25 11:09:03 -0400

    Final commit and push

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,4 +1,22 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class PayrollCalculator {
+    public static void main(String[] args) {
+        Scanner scannerPayRoll = new Scanner(System.in);
+
+        System.out.println("What is your name: ");
+        String name = scannerPayRoll.nextLine();
+
+        System.out.println("Hours worked this week: ");
+        float hoursWorked = scannerPayRoll.nextFloat();
+
+        System.out.println("What is your pay rate?");
+        float payRate = scannerPayRoll.nextFloat();
+
+        float grossPay = hoursWorked * payRate;
+
+        System.out.println("Name: " + name + "\nGross Pay: $" + grossPay);
+    }
 }
